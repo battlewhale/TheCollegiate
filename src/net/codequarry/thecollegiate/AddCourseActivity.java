e55b7847c8113cbdb8355760f831a6555afc48d1
Austin Gulati <contact@austingulati.com>
2012-09-05 21:39:39 -0400

    Finish course add interface

--- src/net/codequarry/thecollegiate/AddCourseActivity.java
+++ src/net/codequarry/thecollegiate/AddCourseActivity.java
@@ -7,34 +7,67 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
+import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Spinner;
 
-public class AddCourseActivity extends Activity {
-
+public class AddCourseActivity extends Activity
+{
+	LinearLayout layout;
+	LayoutInflater layoutInflater;
+	
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_course);
         
         // Add initial blank class
-        LinearLayout layout = (LinearLayout) findViewById(R.id.add_course_classes);
-        LayoutInflater layoutInflator = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        layout.addView(layoutInflator.inflate(R.layout.partial_class_time, null));
+        this.layout = (LinearLayout) findViewById(R.id.add_course_classes);
+        this.layoutInflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.layout.addView(layoutInflater.inflate(R.layout.partial_class_time, null));
         
-        // Populate spinners
-        //Spinner startHourSpinner = (Spinner) findViewById(R.id.add_course_start_hour);
-        //startHourSpinner.setAdapter(new HourAdapter(this));
+        // Add button listeners
+        TimeOnClickListener listener = new TimeOnClickListener();
+        
+        Button startButton = (Button) findViewById(R.id.add_course_start);
+        startButton.setOnFocusChangeListener(listener);
+        startButton.setOnClickListener(listener);
+        
+        Button endButton = (Button) findViewById(R.id.add_course_end);
+        endButton.setOnFocusChangeListener(listener);
+        endButton.setOnClickListener(listener);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         getMenuInflater().inflate(R.menu.activity_add_course, menu);
         return true;
     }
     
-    public void showTimePickerDialog(View v) {
-        DialogFragment newFragment = new TimePickerFragment();
-        newFragment.show(getFragmentManager(), "timePicker");
+    public void addClass(View view)
+    {
+    	this.layout.addView(layoutInflater.inflate(R.layout.partial_class_time, null));
+    }
+    
+    class TimeOnClickListener implements OnClickListener, OnFocusChangeListener
+    {
+    	public void launchDialog(View view)
+    	{
+    		DialogFragment newFragment = new TimePickerFragment(view);
+            newFragment.show(getFragmentManager(), "timePicker");
+    	}
+    	
+    	public void onClick(View view)
+    	{
+    		launchDialog(view);
+    	}
+    	
+    	public void onFocusChange(View view, boolean hasFocus)
+    	{
+    		if(hasFocus) launchDialog(view);
+    	}
     }
 }
--- src/net/codequarry/thecollegiate/TimePickerFragment.java
+++ src/net/codequarry/thecollegiate/TimePickerFragment.java
@@ -1,22 +1,47 @@
 package net.codequarry.thecollegiate;
 
 import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TimePicker;
 
 public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+	Button button;
+	
+	public TimePickerFragment(View view)
+	{
+		this.button = (Button) view;
+	}
 	
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		// Use the current time as the default values for the picker
-		final Calendar c = Calendar.getInstance();
-		int hour = c.get(Calendar.HOUR_OF_DAY);
-		int minute = c.get(Calendar.MINUTE);
+		int hour, minute;
+		
+		CharSequence currentSelection = button.getText();
+		Pattern datePattern = Pattern.compile("([0-9]+):([0-9]+)(a|p)m");
+		Matcher dateMatcher = datePattern.matcher(currentSelection);
+		if(dateMatcher.matches())
+		{
+			// A date has already been selected so lets parse it
+			hour = Integer.parseInt(dateMatcher.group(1));
+			minute = Integer.parseInt(dateMatcher.group(2));
+			if(dateMatcher.group(3).toString().equals("p"))	hour += 12;
+		}
+		else
+		{
+			// Use the current time as the default values for the picker
+			final Calendar c = Calendar.getInstance();
+			hour = c.get(Calendar.HOUR_OF_DAY);
+			minute = c.get(Calendar.MINUTE);
+		}
 		
 		// Create a new instance of TimePickerDialog and return it
 		return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
@@ -24,5 +49,23 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
 	
 	public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
 		// Do something with the time chosen by the user
+		String meridian;
+		if(hourOfDay > 12)
+		{
+			meridian = "pm";
+			hourOfDay -= 12;
+		} else if(hourOfDay == 12)
+		{
+			meridian = "pm";
+		}
+		else
+		{
+			meridian = "am";
+		}
+		String buttonContents = hourOfDay + ":";
+		if(minute < 10) buttonContents += "0";
+		buttonContents += minute + meridian;
+		
+		button.setText(buttonContents);
 	}
 }
